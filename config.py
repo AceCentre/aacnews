b5daa0838847d22c3600aba86a5b142d48d8241f
will wade <willwade@gmail.com>
2014-11-06 20:49:30 +0000

    Update config.py
    update with random stuff

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY ='123456790'
 MAILCHIMP_CAMPAIGN_NAME ='AACNews Monthly'
-MAILCHIMP_APIKEY ='1425524e7dfb0509136e310f1edbed7f-us7'
+MAILCHIMP_APIKEY ='somekeyhere'
 
 DATABASE_FILE ='aacnews_db.sqlite'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, DATABASE_FILE)
 SQLALCHEMY_ECHO =True
 
 # Login Admin
-USERNAME ='willwade@gmail.com'
+USERNAME ='someemail@gmail.com'
 PASSWORD ='pass'
 
