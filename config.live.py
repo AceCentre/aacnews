9a15763c3fc90eea2cfc4432a7864dfc962231e3
Will Wade <willwade@gmail.com>
2014-11-17 09:04:25 +0000

    altered title

--- config.live.py
+++ config.live.py
@@ -4,9 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY ='123456790'
 # Proj Title used in the admin title and for filtering out MC Campaigns in /archive
-PROJECT_TITLE = 'AACNews'
-MAILCHIMP_CAMPAIGN_NAME ='AACNews Monthly'
-MAILCHIMP_BACKUP_CAMPAIGN_NAME ='AACNews Monthly Backup'
+PROJECT_TITLE = 'AACInfo'
+MAILCHIMP_CAMPAIGN_NAME ='AACInfo Monthly'
+MAILCHIMP_BACKUP_CAMPAIGN_NAME ='AACInfo Monthly Backup'
 MAILCHIMP_APIKEY ='mailchimp-api-here'
 
 DATABASE_FILE ='aacnews_db.sqlite'
